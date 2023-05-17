cb8c6057a98d6b978f35de0f720bcdb942c62ca4
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-17 15:14:23 -0300

    feat: configured fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold font-sans">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold font-alt">Sua cápsula do tempo</h1>
     </main>
   )
 }
