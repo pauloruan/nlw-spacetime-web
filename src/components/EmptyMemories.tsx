a501cc0c2a95d48a63adfe300dfb46d1852d6703
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-19 17:41:30 -0300

    feat: `EmptyMemories` component created

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,13 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="text-center leading-relaxed w-[360px]">
+        Você ainda não registrou nenhuma lembrança comece{" "}
+        <a href="" className="underline hover:text-gray-50">
+          comece agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
