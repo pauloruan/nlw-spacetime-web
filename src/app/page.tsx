82fa878fe834a0affd263e903ae98964de38440f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-23 16:15:04 -0300

    feat: Create Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,65 @@
-import { Copyright } from "@/components/Copyright"
 import { EmptyMemories } from "@/components/EmptyMemories"
-import { Hero } from "@/components/Hero"
-import { Profile } from "@/components/Profile"
-import { SignIn } from "@/components/SignIn"
+import { api } from "@/lib/api"
+import dayjs from "dayjs"
+import ptBr from "dayjs/locale/pt-br"
+import { ArrowRight } from "lucide-react"
 import { cookies } from "next/headers"
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Home() {
+dayjs.locale(ptBr)
+
+interface Memory {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+export default async function Home() {
   const isAuthenticated = cookies().has("token")
 
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get("token")?.value
+  const response = await api.get("/memories", {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+
+  const memories: Array<Memory> = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
   return (
-    <main className="grid min-h-screen grid-cols-2 ">
-      <div className="flex flex-col justify-between items-start overflow-hidden px-28 py-16 relative border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
-        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <EmptyMemories />
-      </div>
-    </main>
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => {
+        return (
+          <div key={memory.id} className="space-y-4">
+            <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+              {dayjs(memory.createdAt).format("D[ de ]MMMM[, ]YYYY")}
+            </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={290}
+              className="w-full aspect-video object-cover rounded-lg"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">{memory.excerpt}</p>
+
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
   )
 }
